63a40edcc4bd83097586d70d3d46a87b94671a2c
George <georgewang1994@163.com>
2019-05-23 09:59:48 +0800

    add content

--- books/models.py
+++ books/models.py
@@ -17,6 +17,7 @@ class Books(models.Model):
     name = models.CharField(max_length=100, verbose_name="书的名字")
     desc = models.CharField(max_length=200, verbose_name="书的简介")
     type = models.CharField(max_length=10, default='first', choices=[('first', 'first'), ('second', 'second')], verbose_name="类型")
+    content = models.TextField(default="", verbose_name="书的内容")
     created_time = models.DateTimeField(null=True, auto_now_add=True, verbose_name="创建时间")
 
 
